d16a0782f0c330621242e165e9b8d2eb9b211bfb
fracispime <francisco.ponte.pimentel@student.nhlstenden.com>
2026-09-03 23:14:06 +0200

    pitstop

---
+++ pitstop/pitstop/pitstop.R
@@ -0,0 +1,10 @@
+choice <- readline("Choose a file: ")
+
+file <- read.csv(choice)
+
+num_laps <- nrow(file)
+shortest <- min(file[, 3])
+longest <- max(file[, 3])
+total_time <- sum(file[, 3])
+
+print(paste("There were", num_laps, "pit stops, with the shortest being", shortest, "seconds and the longest being", longest,  "seconds. The total time spent on pit stops was", total_time, "seconds."))
